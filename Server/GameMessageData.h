95536db7abee4c7e1e312f870aaaefdef8e755d8
ForData <1499681247@qq.com>
2021-05-17 09:08:20 +0800

    File

---
+++ Server/GameMessageData.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "MessageData.h"
+
+struct ServerOnlineData
+{
+	std::string m_server_type;
+	std::string m_server_name;
+	std::string m_ip;
+	int m_port;
+};
+
+struct ServerOnlineInfo
+{
+	std::vector<ServerOnlineData> m_vec_server;
+};
+
+struct ServerTest
+{
+	std::vector<int> m_vec_server;
+};
+
+DEFINE_STRUCT_SCHEMA(ServerOnlineData,
+	DEFINE_STRUCT_FIELD(m_server_type, MSG_TYPE::STRING),
+	DEFINE_STRUCT_FIELD(m_server_name, MSG_TYPE::STRING),
+	DEFINE_STRUCT_FIELD(m_ip, MSG_TYPE::STRING),
+	DEFINE_STRUCT_FIELD(m_port, MSG_TYPE::INT)
+);
+
+DEFINE_STRUCT_SCHEMA(ServerOnlineInfo,
+	DEFINE_STRUCT_FIELD(m_vec_server, MSG_TYPE::ARRAY_STRUCT)
+);
+
+DEFINE_STRUCT_SCHEMA(ServerTest,
+	DEFINE_STRUCT_FIELD(m_vec_server, MSG_TYPE::ARRAY_INT)
+);
